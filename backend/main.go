a4eb5e06d87ea6e061ba9363b0972d73eb9bcffc
ho1447 <ho1447@users.noreply.github.com>
2022-11-13 18:23:38 -0500

    feat: frontend connects to backend api

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	docs "CS411-Team-Project/backend/docs"
 	"CS411-Team-Project/backend/handlers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -17,6 +18,9 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+	}))
 	docs.SwaggerInfo.BasePath = "/api/v1"
 
 	v1 := router.Group("/api/v1")
