7d0298b15140292c1e290ec2fccd53c02e2155e4
DanYu12 <dan.daniel.yu@gmail.com>
2022-11-15 15:14:14 -0500

    added recording as well as modal to display more recipe information

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -3,23 +3,23 @@ import RecipeResult from './RecipeResult';
 
 const SearchBar = (props) => {
   const searchBarRef = useRef('');
-  const [responseData, setResponseData] = React.useState('')
-  const [queryText, setQueryText] = React.useState('')
+  const [responseData, setResponseData] = React.useState('');
+  const [queryText, setQueryText] = React.useState('');
 
   // fetch recipes based on parameters
   const fetchData = () => {
-    fetch("http://localhost:8080/api/v1/recipes?query=" + queryText, {
-      method: "GET",
+    fetch('http://localhost:8080/api/v1/recipes?query=' + queryText, {
+      method: 'GET',
     })
-    .then((response) => response.json())
-    .then((response)=>{
-        setResponseData(response)
-        console.log(response)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-  }
+      .then((response) => response.json())
+      .then((response) => {
+        setResponseData(response);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
@@ -36,14 +36,18 @@ const SearchBar = (props) => {
           className="search-bar"
           id="search_bar"
           ref={searchBarRef}
-          onChange={event => setQueryText(event.target.value)}
+          onChange={(event) => setQueryText(event.target.value)}
           placeholder="Enter the recipe"
         />
         <input type="submit" placeholder="Submit" />
       </form>
-
       {/* map search results */}
-      {responseData && <RecipeResult recipeData={responseData}/>}
+      {responseData && (
+        <RecipeResult
+          recipeData={responseData}
+          onRecipeClick={props.onRecipeClick}
+        />
+      )}
     </>
   );
 };
