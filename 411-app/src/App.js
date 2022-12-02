0be8a9cf53468ff95776f6b2ae2cc4c50a5158b0
cmhoyt <cmhoyt@bu.edu>
2022-12-02 10:19:58 -0500

    deleted comments

--- 411-app/src/App.js
+++ 411-app/src/App.js
@@ -1,18 +1,4 @@
-// import './App.css';
-// import Login from './login';
-// import Dashboard from './dashboard';
 
-// const code = new URLSearchParams(window.location.search).get('code')
-
-// function App() {
-//   return (
-//     <div className="app">
-//       {code ? <Dashboard code={code} /> : <Login />}
-//     </div>
-//   );
-// }
-
-// export default App;
 import {useEffect, useState} from "react";
 import './App.css';
 
