19e24acff80d41cf2ec8abc9a5dbd41270a7cc95
nddq <nddangquang@gmail.com>
2022-12-08 19:50:07 -0500

    save recipe functions

--- backend/api/client.go
+++ backend/api/client.go
@@ -1,19 +1,37 @@
 package api
 
 import (
+	"CS411-Team-Project/backend/db"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Client struct {
 	apiKey string
+	db     *mongo.Client
 }
 
 // NewAPIClient returns a new client for invoking the API with provided key
 func NewClient(apiKey string) *Client {
-	return &Client{apiKey: apiKey}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	println("Connected to MongoDB")
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	return &Client{apiKey: apiKey, db: client}
 }
 
 func (client *Client) SearchRecipes(q string) *RecipeSearchResp {
@@ -72,4 +90,31 @@ func (client *Client) GetRecipeInfo(id string) *RecipeInfo {
 		panic(err)
 	}
 	return output
+}
+
+func (client *Client) SaveUserRecipe(userID, recipeID string) error {
+	coll := client.db.Database("cs411db").Collection("savedrecipes")
+	newSaveEntry := db.UserRecipePair{UserID: userID, RecipeID: recipeID}
+	_, err := coll.InsertOne(context.TODO(), newSaveEntry)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (client *Client) GetUserSavedRecipes(userID string) {
+	coll := client.db.Database("cs411db").Collection("savedrecipes")
+	filter := bson.D{{"userID", userID}}
+	cursor, err := coll.Find(context.TODO(), filter)
+	if err != nil {
+		panic(err)
+	}
+	var results []bson.M
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		log.Fatal(err)
+	}
+	for _, result := range results {
+		fmt.Println(result)
+	}
+
 }
